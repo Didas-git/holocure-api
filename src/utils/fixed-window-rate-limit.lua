c3c0bff8db9c509ffd290756ee7649e7359bdf52
DidaS <didasoficial@gmail.com>
2023-09-15 05:08:53 +0100

    Removed non-needed redis call
    Optimized rate limiting

--- src/utils/fixed-window-rate-limit.lua
+++ src/utils/fixed-window-rate-limit.lua
@@ -2,9 +2,8 @@
 
 redis.register_function("fwrl", function (keys, args)
         local hits = redis.call("INCR", keys[1]);
-        local expiresIn = redis.call("PTTL", keys[1]);
 
-        if (expiresIn <= 0) then
+        if (hits <= 1) then
             redis.call("PEXPIRE", keys[1], args[1]);
         end
 
